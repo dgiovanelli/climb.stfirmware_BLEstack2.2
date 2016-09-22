a614f9a4b1654aa635a24e3fcae3405eea2dd60d
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-09-22 09:26:48 +0200

    Upgrade to windows 10 and pc formatting
    This commit contains only modifications needed to make the projects compile after the pc reorganization. It will be followed by another commit

---
+++ ble_sdk_2_02_00_31/tools/scripts/updateVersions.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/sh
+
+VERSIONFILE=versions.txt
+
+#
+#  ======== sedFile ========
+#  Function to search and replace versions
+#  $1 is the file path
+#
+sedFile()
+{
+    workFile=$1
+    echo "Updating $(basename $workFile) ..."
+    sed -i -f $VERSIONFILE $workFile
+}
+
+#
+#  ======== main ========
+#  Main function
+#
+main()
+{
+    files=$(find ../../ -name *.custom_argvars)
+    for fil in $files; do sedFile $fil ; done
+
+    files=$(find ../../ -name *.project)
+    for fil in $files; do sedFile $fil ; done
+
+    files=$(find ../../ -name *.projectspec)
+    for fil in $files; do sedFile $fil ; done
+}
+
+main
+
